ffdd6bca2f50376cb287260cc461168b49a17dd5
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-07-08 12:41:54 -0300

    connection to mongodb with repository and service

--- src/main/java/com/nettuf/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nettuf/workshopmongo/resources/UserResource.java
@@ -1,26 +1,26 @@
 package com.nettuf.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nettuf.workshopmongo.domain.User;
+import com.nettuf.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
 
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
-		User maria = new User("1", "maria silva", "maria@gmail.com");
-		User alex = new User("2", "Alex silva", "alex@gmail.com");
-		List<User> list = new ArrayList<User>();
-		list.addAll(Arrays.asList(maria,alex));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
