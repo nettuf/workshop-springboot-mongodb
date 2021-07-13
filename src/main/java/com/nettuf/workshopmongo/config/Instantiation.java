2f8c1d8f3b367d2320c8f36ba0a54e13cfbf1926
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-07-13 17:09:58 -0300

    data projection of author dto

--- src/main/java/com/nettuf/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nettuf/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.nettuf.workshopmongo.domain.Post;
 import com.nettuf.workshopmongo.domain.User;
+import com.nettuf.workshopmongo.dto.AuthorDTO;
 import com.nettuf.workshopmongo.repository.PostRepository;
 import com.nettuf.workshopmongo.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null,sdf.parse("21/03/2018") , "Partiu viagem", "Vou viajar para sp, abraços", maria);
-		Post post2 = new Post(null,sdf.parse("23/03/2018") , "Bom dia", "que dia lindo", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null,sdf.parse("21/03/2018") , "Partiu viagem", "Vou viajar para sp, abraços", new AuthorDTO(maria));
+		Post post2 = new Post(null,sdf.parse("23/03/2018") , "Bom dia", "que dia lindo",new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
 	}
