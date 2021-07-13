f54c567e80290a114e0fbbca1c9eab393dfe6492
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-07-13 16:36:31 -0300

    creating entity post with user align

--- src/main/java/com/nettuf/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nettuf/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.nettuf.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.nettuf.workshopmongo.domain.Post;
 import com.nettuf.workshopmongo.domain.User;
+import com.nettuf.workshopmongo.repository.PostRepository;
 import com.nettuf.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -15,17 +19,27 @@ public class Instantiation implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post post1 = new Post(null,sdf.parse("21/03/2018") , "Partiu viagem", "Vou viajar para sp, abraços", maria);
+		Post post2 = new Post(null,sdf.parse("23/03/2018") , "Bom dia", "que dia lindo", maria);
+		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-
+		postRepository.saveAll(Arrays.asList(post1,post2));
 		
 	}
 
---
+++ src/main/java/com/nettuf/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.nettuf.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nettuf.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+	
+}
