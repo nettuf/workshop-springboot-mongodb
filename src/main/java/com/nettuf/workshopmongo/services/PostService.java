43fd965ed4d1ad08174b026ea249b5d0cef2557a
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-07-15 13:08:18 -0300

    simple query with query methods

--- src/main/java/com/nettuf/workshopmongo/services/PostService.java
+++ src/main/java/com/nettuf/workshopmongo/services/PostService.java
@@ -25,5 +25,8 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
 	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgonoreCase(text);
+	}
 	
 }
